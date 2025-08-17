151918bc05594b7cb32b5ae2f787b66e5e668db1
Kamil Kohoutek <kamil008@seznam.cz>
2025-08-17 23:08:37 +0200

    Add files via upload

---
+++ BubblePopperTests/BubblePopperTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopperTests.swift
+//  BubblePopperTests
+//
+//  Created by Kamil Kohoutek on 13.08.2025.
+//
+
+import Testing
+@testable import BubblePopper
+
+struct BubblePopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
